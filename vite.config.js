306f4b1c730f934dc2fabb7d1e829949a1c57eb4
ricardo840 <202300006@uprr.edu.mx>
2026-03-16 21:51:39 -0500

    rutas

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         rollupOptions: {
           input: {
             main: resolve(__dirname, 'index.html'),
+            premios: resolve(__dirname, 'premios.html'),
           }
         },
       },
