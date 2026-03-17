d33128757f8fe780658b10495d67deb3e1718565
ricardo840 <202300006@uprr.edu.mx>
2026-03-17 18:02:46 -0500

    index

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         rollupOptions: {
           input: {
             index: resolve(__dirname, 'premios.html'),
+            app: resolve(__dirname, 'index.html'),
             
           }
         },
